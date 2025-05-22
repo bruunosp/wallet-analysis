fa981be85697d2298deb780c9ac2d26c94db65dd
Bruno Passareli <bruno.passareli@hotmail.com>
2025-05-22 02:28:03 -0300

    improvement on after login screen

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,24 +1,36 @@
-import { useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import imgFundo from "../assets/cartao-de-credito-control.jpg";
+import novaImagem from "../assets/mercado-pago.png";
 import { UserContext } from "../context/UserContext";
 
 const Dashboard = () => {
   const { username } = useContext(UserContext);
   const navigate = useNavigate();
+  const [animarTexto, setAnimarTexto] = useState(false);
+  const [imagemFundo, setImagemFundo] = useState(imgFundo);
 
   useEffect(() => {
     if (!username) {
       navigate("/");
     }
+
+    const timer = setTimeout(() => {
+      setAnimarTexto(true);
+      setImagemFundo(novaImagem);
+    }, 4000);
+
+    return () => clearTimeout(timer);
   }, [username, navigate]);
 
   return (
     <div
-      className="main-page__image"
-      style={{ backgroundImage: `url(${imgFundo})` }}
+      className="dashboad-lengths__image animated-bg"
+      style={{ backgroundImage: `url(${imagemFundo})` }}
     >
-      <h1 className="main-page__text">Bem vindo, {username}!</h1>
+      <h1 className={`dashboard-lenghts__text ${animarTexto ? "dashboard-lenghts__move" : ""}`}>
+        Bem vindo, {username}!
+      </h1>
     </div>
   );
 };
